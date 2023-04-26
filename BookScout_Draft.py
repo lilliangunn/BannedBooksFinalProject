59fd9e166dcd9d505ed0656454d72b044747ea7a
kennethyeaher <72979890+kennethyeaher@users.noreply.github.com>
2023-04-25 22:03:40 -0400

    Add files via upload

---
+++ BookScout_Draft.py
@@ -0,0 +1,25 @@
+# Import necessary libraries
+import tkinter
+import pandas as pd
+
+# Define function to search for book
+def search_book():
+    # Read CSV file and preprocess data
+    data = pd.read_csv('')
+    # Remove duplicates and handle missing values
+    data = data.drop_duplicates().fillna('')
+    
+"""what we currently have done""" 
+   
+    # Get user input from GUI
+    
+    # Search for book in dataset
+
+    
+    # Display results to user
+  
+
+# Create GUI
+
+# Start GUI
+
