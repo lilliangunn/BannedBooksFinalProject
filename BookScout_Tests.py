0f4b4f573df50d6eddd5f0e189050a82c6addeb5
kennethyeaher <72979890+kennethyeaher@users.noreply.github.com>
2023-05-11 12:06:26 -0400

    Update BookScout_Tests.py

--- BookScout_Tests.py
+++ BookScout_Tests.py
@@ -1,64 +1,37 @@
-# Load sample data for testing
-data = pd.DataFrame({
-   'title': [
-       'The Catcher in the Rye',
-       'To Kill a Mockingbird',
-       'The Great Gatsby',
-       'Pride and Prejudice',
-       'The Lord of the Rings',
-       'Harry Potter and the Philosopher\'s Stone'
-   ],
-   'author': [
-       'J.D. Salinger',
-       'Harper Lee',
-       'F. Scott Fitzgerald',
-       'Jane Austen',
-       'J.R.R. Tolkien',
-       'J.K. Rowling'
-   ],
-   'publisher': [
-       'Little, Brown and Company',
-       'J. B. Lippincott & Co.',
-       'Charles Scribner\'s Sons',
-       'T. Egerton, Whitehall',
-       'Allen & Unwin',
-       'Bloomsbury Publishing'
-   ]
-})
+import unittest
+import pandas as pd
+from tkinter import END
 
+class TestBookScoutGUI(unittest.TestCase):
+    def setUp(self):
+        # Mock data to simulate the CSV file
+        self.data = pd.DataFrame({
+            'title': ['Joe', 'The Aeronaut\'s Windlass', 'Blood Memory'],
+            'author': ['Larry Brown', 'Jim Butcher', 'Greg Iles'],
+        })
 
-# Test 1: Search for book that exists in dataset (by title)
-search_result = search_books('The Great Gatsby', data, 'title')
-assert len(search_result) == 1
-assert search_result[0] == 'The Great Gatsby'
+        self.app = BookScoutGUI()
+        self.app.data = self.data  # Replace actual data with mock data
 
-# Test 2: Search for book that does not exist in dataset (by title)
-search_result = search_books('The Hobbit', data, 'title')
-assert len(search_result) == 0
+    def test_search_books(self):
+        # Test search by title
+        self.app.input_var.set('title')
+        self.app.input_box.insert(0, 'Joe')
+        result = self.app.search_books()
+        self.assertEqual(result, [('Joe', 'Larry Brown')])
 
-# Test 3: Search for book with partial match (by title)
-search_result = search_books('Harry Potter', data, 'title')
-assert len(search_result) == 1
-assert search_result[0] == 'Harry Potter and the Philosopher\'s Stone'
+        # Test search by author
+        self.app.input_var.set('author')
+        self.app.input_box.delete(0, END)
+        self.app.input_box.insert(0, 'Jim Butcher')
+        result = self.app.search_books()
+        self.assertEqual(result, [('The Aeronaut\'s Windlass', 'Jim Butcher')])
 
-# Test 4: Search for book with case-insensitive match (by title)
-search_result = search_books('the catcher in the rye', data, 'title')
-assert len(search_result) == 1
-assert search_result[0] == 'The Catcher in the Rye'
+        # Test search with no results
+        self.app.input_box.delete(0, END)
+        self.app.input_box.insert(0, 'Nonexistent')
+        result = self.app.search_books()
+        self.assertEqual(result, [])
 
-# Test 5: Search for book by author
-search_result = search_books('jane austen', data, 'author')
-assert len(search_result) == 1
-assert search_result[0] == 'Pride and Prejudice'
-
-# Test 6: Search for book by publisher
-search_result = search_books('Bloomsbury', data, 'publisher')
-assert len(search_result) == 1
-assert search_result[0] == 'Bloomsbury Publishing'
-
-# Test 7: Search for book with missing data
-data.loc[0, 'title'] = None
-search_result = search_books('Catcher', data, 'title')
-assert len(search_result) == 1
-assert search_result[0] == 'The Catcher in the Rye'
-
+if __name__ == '__main__':
+    unittest.main()
