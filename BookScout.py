014c7b1a357bfd2eafb14461b531f72c662accda
lilliangunn <130116395+lilliangunn@users.noreply.github.com>
2023-05-17 12:10:23 -0400

    Most recent versions of bookscout and tests

--- BookScout.py
+++ BookScout.py
@@ -4,39 +4,72 @@ Professor Gabriel Cruz
 
 By: Kenneth Yeaher and Lillian Gunn
 """
+"""GUI to find information about any book from the Goodreads dataset based in its title or author name"""
+
+#pip3 install customtkinter
 
 import tkinter
 import pandas as pd
+import customtkinter
+from customtkinter import StringVar
 
 class BookScoutGUI:
+    """Create GUI and find information based on a type of search term.
+    Methods: search_books(): Search for books in the dataset based on user input.
+            clear_results(): Delete the items in the results textbox.
+            clear_input(): Delete the items in the search entry.
+    """
     def __init__(self):
-        self.window = tkinter.Tk()
+        """
+        Design and return GUI named BookScout"""
+        
+        self.window = customtkinter.CTk()
+        self.window.geometry("400x460")
         self.window.title('BookScout')
-
-        input_label = tkinter.Label(self.window, text='Search by:', width=50) #EDIT:added width=50 to default widen the GUI
+                
+        #appearance of GUI        
+        customtkinter.set_appearance_mode("light")   # Modes: system (default), light, dark
+        customtkinter.set_default_color_theme('dark-blue')  # Themes: blue (default), dark-blue, green
+        
+        #create a frame for search options
+        search_frame = customtkinter.CTkFrame(master=self.window)
+        search_frame.pack(pady=10)
+        #input label
+        input_label = customtkinter.CTkLabel(search_frame, text='Search by:') #removed self.window, width=70
         input_label.pack()
-        self.input_var = tkinter.StringVar()
-        input_dropdown = tkinter.OptionMenu(self.window, self.input_var, 'title', 'author')
+        #dropdown menu
+        self.input_var = customtkinter.StringVar(value="Choose title or author")
+        input_dropdown = customtkinter.CTkOptionMenu(search_frame, values=['title', 'author'], variable=self.input_var) #removed self.window
         input_dropdown.pack()
-
-        input_label = tkinter.Label(self.window, text='Enter search term:')
+    
+        #Create a frame for search term
+        search_term = customtkinter.CTkFrame(self.window)
+        search_term.pack(pady=10)
+        #entry box label
+        input_label = customtkinter.CTkLabel(search_term, text='Enter a search term:') #removed self.window
         input_label.pack()
-        self.input_box = tkinter.Entry(self.window, width=20) #EDIT:width=20 to widen entry box a little
+        #create entry box
+        self.input_box = customtkinter.CTkEntry(search_term, width=200, placeholder_text='Type here') #removed self.window, width=20
         self.input_box.pack()
-
-        search_button = tkinter.Button(self.window, text='Search', command=self.search_books, bg = 'light green') #EDIT:colored search button green
-        search_button.pack()
-
-        result_label = tkinter.Label(self.window, text='Search results:')
+        #Search button for entry box
+        search_button = customtkinter.CTkButton(search_term, text='Search', command=self.search_books, hover_color='green', width=15) #removed self.window, bg = 'light green' 
+        search_button.pack(side=customtkinter.LEFT, padx=10, pady=5)
+        #Clear entry box
+        clear_search_button = customtkinter.CTkButton(search_term, text = 'Clear Search Box', command=self.clear_input, hover_color='red', width=15)
+        clear_search_button.pack(side=customtkinter.LEFT, padx=5, pady=5)
+        
+        #Create a frame for search results
+        results_frame = customtkinter.CTkFrame(self.window)
+        results_frame.pack(pady=5, fill=customtkinter.BOTH, expand=True)
+        #label for search results texbox
+        result_label = customtkinter.CTkLabel(results_frame, text='Search results:') #removed self.window
         result_label.pack()
-        self.result_box = tkinter.Listbox(self.window)
-        self.result_box.pack(fill=tkinter.BOTH, expand=True) #EDIT: added expand to GUI
-
-        #EDIT: added red clear_results button to bottom of GUI; new clear_results() method added also
-        clear_results_button = tkinter.Button(self.window, text='Clear Results', command=self.clear_results, bg='red')
-        clear_results_button.pack()
-
-
+        #create scrollable textbox
+        self.result_box = customtkinter.CTkTextbox(results_frame, activate_scrollbars=True, wrap='none', scrollbar_button_hover_color = 'blue') #removed self.window
+        self.result_box.pack(fill=customtkinter.BOTH, expand=True)
+        #clear results textbox button
+        clear_results_button = customtkinter.CTkButton(results_frame, text='Clear Results', command=self.clear_results, hover_color='red') #removed bg='red'
+        clear_results_button.pack(fill=customtkinter.BOTH, expand=False)
 
         # Load and preprocess data
         try:
@@ -48,6 +81,7 @@ class BookScoutGUI:
 
         self.window.mainloop()
 
+    #Method: search for books
     def search_books(self):
         """
         Search for books in the dataset based on user input.
@@ -57,22 +91,47 @@ class BookScoutGUI:
         """
         search_column = self.input_var.get()
         user_input = self.input_box.get()
+        
+
+        #check if a search option has been selected
+        if search_column == 'Choose title or author':
+            self.result_box.delete("0.0", customtkinter.END)
+            self.result_box.insert(customtkinter.END, "STOP! You must first choose to search for a title or an author in the option menu.")
+            return[]
+        
+        #check that user input is present
+        if not user_input.strip():
+            self.result_box.delete("0.0", customtkinter.END)
+            self.result_box.insert(customtkinter.END, "STOP! You must pick a search option and then enter a search term.")
+            return[]
 
         # Search for book in dataset
         result = self.data[self.data[search_column].str.contains(user_input, na=False, case=False)]
 
         # Display results to user
-        self.result_box.delete(0, tkinter.END)
+        self.result_box.delete("0.0", customtkinter.END)
+        #insert count of total number of results at the top
+        self.result_box.insert(customtkinter.END, f"Total results: {len(result)}\n\n")
+        #what to return
         for index, row in result.iterrows():
-            self.result_box.insert(tkinter.END, f"{row['title']} by {row['author']}")
+            self.result_box.insert(customtkinter.END, f"{row['title']} by {row['author']}\n        Date Published: {row['date_published']}\n        Publisher: {row['publisher']}\n        Series: {row['series']}\n        Total number of ratings: {row['rating_count']}\n        Average Rating: {row['average_rating']}\n        Five Star Ratings: {row['five_star_ratings']}\n        One Star Ratings: {row['one_star_ratings']}\n        Awards: {row['awards']}\n        Genre and votes: {row['genre_and_votes']}\n        Goodreads Link: {row['link']}  \n\n")
+        #return
+        return [(row['title'], row['author'], row['average_rating']) for _, row in result.iterrows()]
 
-        return [(row['title'], row['author']) for _, row in result.iterrows()]
-
-    #EDIT: new clear_results method
+    #new clear_results method for textbox
     def clear_results(self):
         """
-        Delete the items in the results box."""
-        self.result_box.delete(0, tkinter.END)
+        Delete the items in the results textbox.
+        """
+        self.result_box.delete("0.0", customtkinter.END)
+    
+    #clear input method for entry box
+    def clear_input(self):
+        """
+        Delete the items in the search entry.
+        """
+        self.input_box.delete(0, customtkinter.END)
+    
 
 if __name__ == '__main__':
     app = BookScoutGUI()
--- BookScout_Tests.py
+++ BookScout_Tests.py
@@ -1,5 +1,6 @@
 import unittest
 import pandas as pd
+from customtkinter import END
 from tkinter import END
 
 class TestBookScoutGUI(unittest.TestCase):
@@ -8,6 +9,16 @@ class TestBookScoutGUI(unittest.TestCase):
         self.data = pd.DataFrame({
             'title': ['Joe', 'The Aeronaut\'s Windlass', 'Blood Memory'],
             'author': ['Larry Brown', 'Jim Butcher', 'Greg Iles'],
+            'date_published': ['1980', 'May 15th, 2020', 'June 2003']
+            'publisher': ['Penguin', 'Robber Baron', 'Greenleaf']
+            'series': ['(Chosen ones)', '(Boring books)', '(Cool Books)']
+            'rating_count': ['111', '32', '5']
+            'average_rating': ['3.4', '5', '2.3']
+            'five_star_ratings': ['55', '3', '2']
+            'one_star_ratings': ['30', '10', '0']
+            'awards':['Trust 2009', 'Lifetime 2020',  'Teens 2004']
+            'genre_and_votes': ['Children 11', 'Action 13, Romance 55', 'Action 6, Grief 44']
+            'link':['abcd.com', 'efghi.com', 'jklmno.com']
         })
 
         self.app = BookScoutGUI()
@@ -18,20 +29,32 @@ class TestBookScoutGUI(unittest.TestCase):
         self.app.input_var.set('title')
         self.app.input_box.insert(0, 'Joe')
         result = self.app.search_books()
-        self.assertEqual(result, [('Joe', 'Larry Brown')])
+        self.assertEqual(result, [('Joe', 'Larry Brown', '1980', 'Penguin', '(Chosen ones)', '111', '3.4', '55', '30', 'Trust 2009', 'Children 11', 'abcd.com' )])
 
         # Test search by author
         self.app.input_var.set('author')
         self.app.input_box.delete(0, END)
         self.app.input_box.insert(0, 'Jim Butcher')
         result = self.app.search_books()
-        self.assertEqual(result, [('The Aeronaut\'s Windlass', 'Jim Butcher')])
+        self.assertEqual(result, [('The Aeronaut\'s Windlass', 'Jim Butcher', 'May 15th, 2020', 'Robber Baron', '(Boring books)', '32', '5', '3', '10', 'Lifetime 2020', 'Action 13, Romance 55', 'efghi.com')])
 
         # Test search with no results
         self.app.input_box.delete(0, END)
         self.app.input_box.insert(0, 'Nonexistent')
         result = self.app.search_books()
         self.assertEqual(result, [])
+        
+    def test_clear_results(self):
+        self.app.input_var.set('title') #reset the search column
+        self.app.input_box.insert(0, 'Joe')
+        self.app.search_books()
+        self.app.clear_results()
+        self.assertEqual(self.app.result_box.get('1.0', END), '\n')
+        
+    def test_clear_input(self):
+        self.app.input_box.insert(0, 'Joe')
+        self.app.clear_input()
+        self.assertEqual(self.app.input_box.get(), '')
 
 if __name__ == '__main__':
     unittest.main()
